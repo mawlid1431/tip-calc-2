5e1e9cd0545b8d581bb430f9139ced75556fbe88
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 14:53:20 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function calcTip(bill) {
+  let tip;
+  
+  if (bill >= 50 && bill <= 300) {
+    tip = bill * 0.15; // 15% tip
+  } else {
+    tip = bill * 0.20; // 20% tip
+  }
+  
+  const total = bill + tip;
+  
+  console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+}
+
+// Example usage:
+calcTip(275); // Output: "The bill was 275, the tip was 41.25, and the total value 316.25"
 console.log("hello");
